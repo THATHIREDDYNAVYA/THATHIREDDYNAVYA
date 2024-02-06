bd66f80a8145abce9d381e6f77fb8de5216d33a9
THATHIREDDYNAVYA <159098383+THATHIREDDYNAVYA@users.noreply.github.com>
2024-02-06 08:46:26 +0530

    Add files via upload

---
+++ cl1d.py
@@ -0,0 +1,8 @@
+a=2
+b=3
+c=a+b
+print(c)
+n=int(input('enter the first number'))
+m=int(input('enter the second number'))
+c=n+m
+print('the sum is :',c)
---
+++ l1.py
@@ -0,0 +1,19 @@
+list1=list()
+list2=list()
+for i in range(0,3):
+ n=int(input('enter a number:'))
+ list1.append(n)
+ m=int(input('enter a number:'))
+ list2.append(m)
+sum=0
+print(list1)
+print(list2)
+for i in range(0,3):
+  sum+=(list1[i]*list2[i])
+print(sum)
+ 
+ 
+
+
+ 
+
